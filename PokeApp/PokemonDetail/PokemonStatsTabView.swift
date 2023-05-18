c396f0863a769df12693f539dbfcca851b3dcada
cloudfelipe <cloud.felipe@gmail.com>
2023-05-18 01:28:30 -0500

    WIP Pokemon Detail Tab Bar

--- PokeApp/PokemonDetail/PokemonStatsTabView.swift
+++ PokeApp/PokemonDetail/PokemonStatsTabView.swift
@@ -33,47 +33,78 @@ enum PokemonDetailTab: String, CaseIterable, Identifiable {
 
 struct PokemonStatsTabView: View {
     
+    @Namespace var namespace
+    
     @Binding var selectedTab: PokemonDetailTab
+    
+    @State private var selectedTabItemFrame: CGRect = .zero
+    @State private var tabItemFrames: [CGRect] = [.zero,.zero,.zero,.zero]
     
     var body: some View {
         HStack {
             content
         }
-        .padding(.vertical)
-        .mask {
-            RoundedRectangle(cornerRadius: 24.0, style: .continuous)
-        }
+        .overlay (
+            overlay
+        )
+        .coordinateSpace(name: "TabView")
     }
     
     private var content: some View {
-        ForEach(PokemonDetailTab.allCases) { tabItem in
+        ForEach(Array(PokemonDetailTab.allCases.enumerated()), id: \.offset) { index, tabItem in
             Button {
-                withAnimation(.easeOut) {
-                    selectedTab = tabItem
+                withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                    self.selectedTab = tabItem
+                    self.selectedTabItemFrame = tabItemFrames[index]
                 }
             } label: {
                 Text(tabItem.title)
                     .customFont(.subheadline2)
-                    .foregroundColor(.black)
-                    .opacity(selectedTab == tabItem ? 1 : 0.5)
-                    .background(
-                        VStack (spacing: 15.0){
-                            Spacer()
-                            RoundedRectangle(cornerRadius: 2)
-                                .foregroundColor(.purple.opacity(0.7))
-                                .frame(width: selectedTab == tabItem ? 50.0 : 0.0, height: 4.0)
-                                .offset(y: 8)
-                            .opacity(selectedTab == tabItem ? 1 : 0.0)
+                    .frame(height: 40.0)
+                    .overlay {
+                        GeometryReader { proxy in
+                            let frame = proxy.frame(in: .named("TabView"))
+                            Color.clear
+                                .preference(key: TabBarItemPreferenceFrameKey.self, value: frame)
+                                .onPreferenceChange(TabBarItemPreferenceFrameKey.self) { value in
+                                    tabItemFrames[index] = value
+                                    //used for positioning first element when first run
+                                    if selectedTab == tabItem {
+                                        self.selectedTabItemFrame = tabItemFrames[index]
+                                    }
+                                }
                         }
-                    )
+                    }
             }
-            .frame(maxWidth: .infinity, idealHeight: 40.0)
+            .foregroundColor( selectedTab == tabItem ? .primary : .secondary)
             
+            //TODO: Calculate last index instead to check item directly
+            if tabItem != .moves { Spacer() }
         }
+        
+    }
+    
+    private var overlay: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 2)
+                .foregroundColor(.purple.opacity(0.7))
+                .frame(width: selectedTabItemFrame.width, height: 4.0)
+                .position(x: selectedTabItemFrame.midX, y: selectedTabItemFrame.maxY)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+    }
+}
+
+private struct TabBarItemPreferenceFrameKey: PreferenceKey {
+    static var defaultValue: CGRect = .zero
+    
+    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
+        value = nextValue()
     }
 }
 
 struct PokemonStatsView_Previews: PreviewProvider {
+    
     static var previews: some View {
         PokemonStatsTabView(selectedTab: .constant(.about))
     }
